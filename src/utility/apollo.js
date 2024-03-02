86efbc6958855c0e85809e848956e130838f8c8b
Elias Amaral <eliasseverianoamaral@gmail.com>
2024-03-02 12:16:11 -0300

    devTools config

--- src/utility/apollo.js
+++ src/utility/apollo.js
@@ -9,6 +9,7 @@ import { setContext } from "@apollo/client/link/context";
 const API_URL = import.meta.env.VITE_API_URL;
 
 
+
 const httpLink = createHttpLink({
   uri: API_URL,
 });
@@ -27,6 +28,7 @@ const link = from([authLink, httpLink]);
 const client = new ApolloClient({
   link,
   cache: new InMemoryCache(),
+  connectToDevTools: process.env.NODE_ENV === "development",
 });
 
 export default client;
