4cb1ca6690855f4fe628a5c876d37698d8d9040c
Elias Amaral <eliasseverianoamaral@gmail.com>
2024-03-24 22:48:28 -0300

    refatoração

--- src/utility/process_ads_data.js
+++ src/utility/process_ads_data.js
@@ -1,52 +1,63 @@
-import * as XLSX from "xlsx";
+import * as XLSX from 'xlsx'
 
-export function processarArquivo(arquivo) {
+export function extractServicesAndMaterialsADS(arquivo) {
   return new Promise((resolve, reject) => {
-    const input = arquivo.target.files[0];
-    const leitor = new FileReader();
+    const input = arquivo.target.files[0]
+    const leitor = new window.FileReader()
 
     leitor.onload = ({ target: { result } }) => {
-      const pastaDeTrabalho = XLSX.read(result, { type: "binary" });
-      const planilha = pastaDeTrabalho.Sheets[pastaDeTrabalho.SheetNames[1]];
-      const dados = XLSX.utils.sheet_to_json(planilha, { header: 2 });
-      resolve(dados);
-    };
+      const pastaDeTrabalho = XLSX.read(result, { type: 'binary' })
+      const planilha = pastaDeTrabalho.Sheets[pastaDeTrabalho.SheetNames[1]]
+      const dados = XLSX.utils.sheet_to_json(planilha, { header: 2 })
+
+      const codesFilteredByColumns = dados.map((item) => ({
+        codigo: item['Código'],
+        descricao: item['Descrição'],
+        unid: item['Unid.'],
+        qntOrcada: item.Planejado,
+      }))
+
+      const removeEmptyLines = codesFilteredByColumns.filter(
+        (objeto) =>
+          objeto.codigo !== undefined || objeto.descricao !== undefined,
+      )
+
+      const codigosAgrupadosESomados = removeEmptyLines.reduce(
+        (agrupado, item) => {
+          const { codigo, qntOrcada } = item
+
+          if (agrupado[codigo]) {
+            agrupado[codigo].qntOrcada += qntOrcada
+          } else {
+            agrupado[codigo] = { ...item, qntOrcada }
+          }
+
+          return agrupado
+        },
+        {},
+      )
+      const codigosAgrupadosESomadosArray = Object.values(
+        codigosAgrupadosESomados,
+      )
+
+      const materiais = []
+      const servicos = []
+
+      codigosAgrupadosESomadosArray.forEach((obj) => {
+        if (obj.unid === 'SRV') {
+          servicos.push({ ...obj, unid: undefined })
+        } else {
+          materiais.push({ ...obj, unid: undefined })
+        }
+      })
+
+      resolve({ materiais, servicos })
+    }
 
     leitor.onerror = (erro) => {
-      reject(erro);
-    };
+      reject(erro)
+    }
 
-    leitor.readAsBinaryString(input);
-  });
+    leitor.readAsBinaryString(input)
+  })
 }
-export const filter = (array, unidade) => {
-  const itensFiltradosPorSRV = array.filter(
-    (item) => item["Unid."] === unidade
-  );
-
-  const codigosFiltrosPorChave = itensFiltradosPorSRV.map((item) => ({
-    codigo: item["Código"],
-    descricao: item["Descrição"],
-    qntOrcada: item["Planejado"],
-    // valor: item["Valor Material"],
-  }));
-
-  const codigosAgrupadosESomados = codigosFiltrosPorChave.reduce(
-    (agrupado, item) => {
-      const { codigo, qntOrcada } = item;
-
-      if (agrupado[codigo]) {
-        agrupado[codigo].qntOrcada += qntOrcada;
-        // agrupado[codigo].valor += valor; // Sum the "valor" field as well
-      } else {
-        agrupado[codigo] = { ...item, qntOrcada };
-      }
-
-      return agrupado;
-    },
-    {}
-  );
-
-  const codigosFinal = Object.values(codigosAgrupadosESomados);
-  return codigosFinal;
-};
